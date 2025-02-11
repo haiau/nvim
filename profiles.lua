5c57e2186c071dbd67c8d06d25c9c8e3b3f06bfd
Pham Nguyen Hai Au <pnhaiau@gmail.com>
2025-02-11 12:36:35 +0700

    Add astronvim and p2

--- profiles.lua
+++ profiles.lua
@@ -11,28 +11,39 @@
      \  \::/       \  \:\        \  \::/       \  \::/      \__\::::/      \__\/      \  \:\    
       \__\/         \__\/         \__\/         \__\/           ~~~~                   \__\/    
 
-	A config switcher written in Lua by NTBBloodbath and Vhyrro.
+  A config switcher written in Lua by NTBBloodbath and Vhyrro.
 --]]
 
 -- Defines the profiles you want to use
 local profiles = {
-	--[[
-	Here's an example:
+  --[[
+  Here's an example:
 
-		<name_of_config> = { <path_to_config>, {
-				plugins = "packer", -- Where to install plugins under site/pack
-				preconfigure = "packer:opt" -- Whether or not to preconfigure a plugin manager for you
-			}
-		}
+    <name_of_config> = { <path_to_config>, {
+        plugins = "packer", -- Where to install plugins under site/pack
+        preconfigure = "packer:opt" -- Whether or not to preconfigure a plugin manager for you
+      }
+    }
 
-	More in-depth information can be found in cheovim's README on GitHub.
-	--]]
-	my_config = { "~/.config/nvim/nvchad", {
-			plugins = "packer",
-			preconfigure = "packer",
-		}
-	},
+  More in-depth information can be found in cheovim's README on GitHub.
+  --]]
+  my_config = { "~/.config/nvim/nvchad", {
+      plugins = "packer",
+      preconfigure = "packer",
+    }
+  },
+  p2 = { "~/.config/nvim/p2", {
+      plugins = "packer",
+      preconfigure = "packer",
+    } 
+  },
+  astronvim = { "~/.config/nvim/astronvim/", {
+      plugins = "packer",
+      preconfigure = "packer",
+    } 
+  },
+
 }
 
 -- return <name_of_config>, <list_of_profiles>
-return "my_config", profiles
+return "astronvim", profiles
